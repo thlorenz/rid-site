00a50133cd78620e5217c31d9701eec2f63b5297
Thorsten Lorenz <thlorenz@gmx.de>
2021-05-13 17:35:24 -0600

    static: updating images and manifest

--- static/js/main.js
+++ static/js/main.js
@@ -1,13 +1,12 @@
 // Set darkmode
-document.getElementById('mode').addEventListener('click', () => {
-
-    document.body.classList.toggle('dark');
-    localStorage.setItem('theme', document.body.classList.contains('dark') ? 'dark' : 'light');
-  
+document.getElementById("mode").addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  localStorage.setItem(
+    "theme",
+    document.body.classList.contains("dark") ? "dark" : "light"
+  );
 });
-  
-if (localStorage.getItem('theme') === 'dark') {
-  
-  document.body.classList.add('dark');
-  
+
+if (localStorage.getItem("theme") !== "light") {
+  document.body.classList.add("dark");
 }
